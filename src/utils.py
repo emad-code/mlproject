23ec3549f2117b7efa1bf5e89c936bbb2d73c8e1
emad-code <emadahmedc16@gmail.com>
2024-02-07 12:45:00 +0000

    Hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -11,8 +11,10 @@ import pandas as pd
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
+from src.logger import logging
 
 
 
@@ -28,11 +30,20 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
+
+            # HP tuning
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            logging.info("HP tuning complete")
+
+            model.set_params(**gs.best_params_)   # ** unpacks dict into args
             
             model.fit(X_train, y_train)     # train model
 
