fc71d7bde13cf4624b213bac4319db4cf06bdf30
emad-code <emadahmedc16@gmail.com>
2024-04-22 15:29:53 +0100

    Updated descriptions for easy understanding.

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
 # This file serves as a marker, indicating to Python that it should be 
-# treated as a package. This allows module importation
-# e.g. import package.module
+# treated as a package. This happens when setup.py is run;
+# It looks for a __init__.py file in each folder.
+# This allows module importation e.g. import package.module
